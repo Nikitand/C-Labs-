fcbc6d812c22fee81415e913e6b871db9f4b3f8f
Nikitand <nikitos_by_brest@mail.ru>
2019-10-29 01:28:59 +0300

    Laba 6

---
+++ №6/Laba6/Laba6/Class1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAba5
+{
+    public abstract partial  class Animal
+    {
+        public int Age;
+    }
+
+
+    enum list
+    {
+        first = 1,
+        second,
+        third,
+        fourth,
+        fifth
+    }
+
+    public class ZOO
+    {
+        public Base[]  an;
+        public int count = 0;
+        public int size;
+      //  list list = new list();
+
+        public ZOO()
+        {
+            size = 5;
+            an = new Base[5];
+        }
+
+        public bool isFull()
+        {
+            return (count == size);
+        }
+
+        public bool isEmpty()
+        {
+            return (count == 0);
+        }
+
+        public void Add(Base el)
+        {
+            if (isFull())
+                return;
+            an[count++] = el;
+        }
+
+        public void Del(Base el)
+        {
+            int num = 0;
+            if (isEmpty())
+                return;
+            for (int i = 0; i < count; i++)
+            {
+                if (an[i].Equals(el))
+                    num = i;
+            }
+            for (int i = num; i < count; i++)
+            {
+                an[i] = an[i + 1];
+            }
+            count--;
+        }
+
+        public void show()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(an[i].ToString());
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public abstract class Base
+    {
+        public struct Info
+        {
+            public int Age;
+        }
+        public Info info = new Info();
+    }
+
+    public static class controller
+    {
+
+
+        public static     void counter(ZOO animal)
+        {
+            int count;
+        
+        }
+
+        public static void sort(ZOO animal)
+        {
+            Base elem;
+            for (int i = 0; i < animal.count - 1; i++)
+            {
+                for (int j = 0; j <    animal.count - i - 1; j++)
+                {
+                    if (animal.an[j].info.Age < animal.an[j + 1].info.Age)
+                    {
+                        elem = animal.an[j];
+                        animal.an[j] = animal.an[j + 1];
+                        animal.an[j + 1] = elem;
+                    }
+                }
+            }
+        }
+
+        public static void show(ZOO animal)
+        {
+            string[]  Kind = new string[6];
+            string[] Name = new string[6];
+            int pos = 0;
+            bool b = false;
+            for (int j = 0; j < animal.count; j++)
+            {
+                for (int i = 0; i < pos; i++)
+                {
+                    if (Kind[i] == (animal.an[j].GetType()).Name)
+                    {
+                        b = true;
+                      //  count[i]++;
+                        break;
+                    }
+                }
+                if (!b)
+                {
+                    Kind[pos] = animal.an[j].GetType().Name;
+                    //count[pos]++;
+                    pos++;
+                }
+                b = false;
+            }
+
+            Console.WriteLine("Info:");
+            for (int i = 0; i < pos; i++)
+            {
+                Console.WriteLine($"{Kind[i]} - {Name[i]}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ №6/Laba6/Laba6/Program.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAba5
+{
+   
+    interface ISound
+    {
+        void Sound();
+    }
+    interface Iinter
+    {
+        string ToString();
+    }
+
+   
+
+    public abstract partial class Animals: Base
+        { 
+
+
+        public virtual string Kind_of_animal { get; set; } // Вид животного
+        public string Habitat;
+        public void str()
+        {
+            Habitat = null;
+        }
+       public  int Age;
+
+        public override string ToString()
+        {
+            return this.Kind_of_animal + this.Habitat;
+        }
+
+        abstract public void Sound();
+    }
+
+    public class Mammial : Animals, ISound
+    {
+        public override string Kind_of_animal
+        {
+            get => base.Kind_of_animal;
+            set => base.Kind_of_animal = value;
+        }
+        public virtual void Name()
+        {
+        }
+        public override string ToString()
+        {
+            return this.Kind_of_animal;
+        }
+
+        public override void Sound()
+        {
+
+        }
+
+    }
+    public class Birds : Animals
+    {
+        public override string Kind_of_animal
+        {
+            get => base.Kind_of_animal;
+            set => base.Kind_of_animal = value;
+        }
+        public virtual void Name()
+        {
+        }
+        public override string ToString()
+        {
+            return this.Kind_of_animal;
+        }
+        public override void Sound()
+        {
+
+        }
+    }
+    public class Fish : Animals, Iinter
+    {
+        public override string Kind_of_animal
+        {
+            get => base.Kind_of_animal;
+            set => base.Kind_of_animal = value;
+        }
+        public virtual void Name()
+        {
+        }
+        public override string ToString()
+        {
+            return this.Habitat;
+
+        }
+        public override void Sound()
+        {
+
+        }
+    }
+    // Бесплоный класс
+    sealed class Crocodile : Animals, ISound, Iinter
+    {
+        readonly string Name = "Croki";
+        readonly string Kind = "Reptile";
+        private new readonly string Habitat = "Tropics";
+
+
+        public override void Sound()
+        {
+            Console.WriteLine("Араррараарар");
+        }
+        void ISound.Sound()
+        {
+            Console.WriteLine(" Реализация функции интерфейса");
+        }
+    }
+
+
+    public class Lion : Mammial, ISound, Iinter
+    {
+
+
+        public override void Name()
+        {
+            Console.WriteLine("Leo");
+        }
+        public override void Sound()
+        {
+            Console.WriteLine("Рррррр");
+        }
+
+    }
+    public class Tigr : Mammial, ISound, Iinter
+    {
+
+        public override void Name()
+        {
+            Console.WriteLine("Tigra");
+        }
+        public override void Sound()
+        {
+            Console.WriteLine("Рррррр");
+        }
+    }
+    public class Owl : Birds, ISound
+    {
+
+        public override void Name()
+        {
+            Console.WriteLine("Sova");
+        }
+        public override void Sound()
+        {
+            Console.WriteLine("Оу Оу");
+        }
+    }
+    public class Parrot : Birds
+    {
+
+        public override void Name()
+        {
+            Console.WriteLine("Kesha");
+        }
+        public override void Sound()
+        {
+            Console.WriteLine("Привет, я попугай Кеша");
+        }
+
+        public class Shark : Fish, Iinter
+        {
+
+            public override void Name()
+            {
+
+                Console.WriteLine("Nemo");
+            }
+
+        }
+        
+        // /print
+
+        class Print
+        {
+            public static void IAmPrinting(Iinter obj)
+            {
+                Console.WriteLine(obj.ToString());
+            }
+        }
+
+
+        class Program
+        {
+
+            static void Main(string[] args)
+
+            {
+                //int a = 8;
+                ZOO zoo = new ZOO();
+                Mammial[] mammials = new Mammial[2];
+                Lion Lion = new Lion();
+                Lion.Kind_of_animal = "Mammial";
+                Lion.Habitat = "Savanna";
+                Tigr Tigra = new Tigr();
+                Tigra.Kind_of_animal = "Mammial";
+                Tigra.Habitat = "Savanna";
+                mammials[0] = Lion;
+                mammials[1] = Tigra;
+                Fish fish = new Fish()
+                {
+                    Kind_of_animal = "Fish",
+                    Habitat = "Ocean"
+                };
+                Crocodile croky = new Crocodile();
+                ISound sou = croky;
+                Console.WriteLine("Croky");
+                Console.Write("Голос:");
+
+                croky.Sound();
+                zoo.Add(Lion);
+                zoo.Add(Tigra);
+                zoo.Add(fish);
+                zoo.Add(croky);
+                zoo.show();
+                controller.show(zoo);
+                controller.sort(zoo);
+
+                ///
+
+
+                //////
+           
+
+
+                ////////
+                /////
+                //Console.WriteLine();
+                //Console.WriteLine(mammials.ToString());
+                //Console.WriteLine(fish.ToString());
+                //Console.WriteLine();
+                ////     is/as
+                //bool value = Lion is int;
+                //Console.WriteLine(value);
+                //Console.WriteLine(a as Object);
+                //  
+                //Console.WriteLine();
+                //foreach (Iinter x in arr)
+                //{
+                //    Print.IAmPrinting(x);
+                //}
+            }
+        }
+    }
+}

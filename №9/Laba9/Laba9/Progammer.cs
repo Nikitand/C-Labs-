8605c46ccc7be0308c263b7aa114513460f9b7ba
Nikitand <nikitos_by_brest@mail.ru>
2019-11-26 01:07:14 +0300

    Laba 9

---
+++ №9/Laba9/Laba9/Progammer.cs
@@ -0,0 +1,86 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba9
+{
+    class ProgCont
+    { 
+            public event EventHandler Rename;
+            public event EventHandler New_property;
+            public void Run()
+            {
+            Console.WriteLine( "Run");
+            if (Rename != null)
+                Rename(this, null);
+            New_property?.Invoke(this, null);
+            }
+
+    }
+    class Language
+    {
+       
+            static int count = 0;
+             public int Property;  
+            public string Name;
+
+
+            public Language()
+            {
+                Name = "C# " ;
+            Property = 15;
+            count++;
+            }
+
+            public Language(int lv)
+            {
+                Name = "Jav ";
+                Property = lv;
+            count++;
+            }
+
+            public Language(int vl, string nam)
+            {
+                Name = nam ;
+                Property = vl;
+            count++;
+            }
+
+            ~Language()
+            {
+                count--;
+            }
+
+            public void Newname(object sender, EventArgs e)
+            {
+            int i = 0;
+            Console.WriteLine("Язык " +  Name + "\n Если название верно нажми 1, если нет нажми 0");
+            i = Convert.ToInt32(Console.ReadLine());
+            if (i == 1)
+            { return; }
+            else if (i == 0)
+            {
+                Console.WriteLine("Введите правильное имя языка");
+                Name = Console.ReadLine();
+               
+            }
+            }
+
+            public void change(object sender, EventArgs e)
+            {
+            if (Property > 5)
+            {
+                Console.WriteLine("Добавляем  свойства");
+                Property--;
+            }
+            else {
+                Console.WriteLine("Удаляем свойство");
+                Property++;
+            }
+            }
+        
+    }
+}

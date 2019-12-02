390c4bf14ef3789beef2e0f8e38588343246dbb2
Nikitand <nikitos_by_brest@mail.ru>
2019-12-03 02:38:23 +0300

    laba

---
+++ №11/Laba11/Laba11/Plane.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba11
+{
+    public partial class Airline
+    {
+        public uint makeHash(string des, int num, string t, weekDays day)
+        {
+            int intRes = des.Length + num * 9 - t.Length;
+            if (day > weekDays.thir)
+                intRes *= 54;
+            uint res = (uint)intRes;
+            return res;
+        }
+
+
+
+        public override bool Equals(object obj)
+        {
+            var Air = obj as Airline;
+
+            if (Air == null)
+            {
+                return false;
+            }
+
+            return Air.NUMBER == NUMBER && Air.type == TYPE;
+        }
+
+        public override string ToString()
+        {
+            return "AIRLINE: " + "Тип"+ TYPE + "\t" +  "№ "+NUMBER + "\t" +"г." +  DESTINATION +"\t" + time +  "часа(ов)";
+        }
+
+    }
+    partial class Airline
+    {
+        public string destination = "";
+        public int number = 0;
+        public string type = "";
+        public int time = 0;
+        public enum weekDays
+        {
+            sun = 1, mon, thue, wen, thir, fri, sat
+        }
+        private weekDays wd = weekDays.mon;
+        private readonly uint FlightID;
+        private static int size = 0;
+        public static void sizeinfo()
+        {
+            Console.WriteLine("Size:" + size);
+        }
+
+        public string DESTINATION
+        {
+            get { return this.destination; }
+            set { this.destination = value; }
+
+        }
+        public int NUMBER
+        {
+            get { return this.number; }
+            set { this.number = value; }
+
+        }
+        public string TYPE
+        {
+            get { return this.type; }
+            set { this.type = value; }
+
+        }
+        public weekDays DAY
+        {
+            get { return this.wd; }
+            set { this.wd = value; }
+
+        }
+        public uint ID
+        {
+            get { return this.FlightID; }
+
+        }
+        public Airline()
+        {
+            this.destination = "no";
+            this.number = 0;
+            this.type = "no";
+            this.time = 0000;
+            this.wd = weekDays.sun;
+            this.FlightID = makeHash(destination, number, type, wd);
+            size++;
+            Console.WriteLine("Конструктор без параметров!");
+
+        }
+
+        public Airline(string des, int n, string ty, int t, weekDays day)
+        {
+            this.destination = des;
+            this.number = n;
+            this.type = ty;
+            this.time = t;
+            this.wd = day;
+            this.FlightID = makeHash(destination, number, type, wd);
+            size++;
+            Console.WriteLine("Конструктор с параметрами!");
+        }
+
+        static Airline()
+        {
+            Console.WriteLine("Статический конструктор!");
+        }
+        public Airline(string des, int n, string ty)
+        {
+            this.destination = des;
+            this.number = n;
+            this.type = ty;
+            this.FlightID = makeHash(destination, number, type, wd);
+            size++;
+            Console.WriteLine("Просто конструктор!");
+        }
+        public Airline(string des, int n, string ty, int t)
+        {
+            this.destination = des;
+            this.number = n;
+            this.type = ty;
+            this.time = t;
+            
+        }
+        }
+}
---
+++ №11/Laba11/Laba11/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Laba11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ограничении длины");
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());
+            string[] str = new string[12] { "January", "Febraury", "March", "April", "May", "June", "Jule", "August", "September", "October", "November", "December" };
+            IEnumerable<string> month = from i in str
+                                        where i.Length < n
+                                        select i;
+            foreach (string i in month)
+                Console.WriteLine(i);
+
+            Console.WriteLine();
+            Console.WriteLine("Зимниие и летние месяцы  года");
+            IEnumerable<string> winterorsummer = str
+                                                 .Take(2)
+                                                 .Concat(str.Skip(5).Take(3))
+                                                  .Concat(str.Skip(11).Take(1));
+            foreach (string i in winterorsummer)
+                Console.WriteLine(i);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Алфавит");
+            IEnumerable<string> Alpahibt = str
+                                           .OrderBy(i => i);
+                                         
+            foreach (string i in Alpahibt)
+                Console.WriteLine(i);
+            Console.WriteLine();
+            Console.WriteLine("Буква u  и месяцы длинной не меньше 4 букв");
+            IEnumerable<string> SpecCondition = from i in str
+                                                where (i.Contains("u") && i.Length >= 4)
+                                                select i;
+            foreach (string i in SpecCondition)
+                Console.WriteLine(i );
+            Console.WriteLine();
+
+            List<Airline> Plane = new List<Airline>();
+            Plane.Add(new Airline("Minsk", 123, "Boeing", 2));
+            Plane.Add(new Airline("Kair", 789, "Airbus", 3));
+            Plane.Add(new Airline("Amsterdam", 054, "Shark", 10));
+            Plane.Add(new Airline("New York", 147, "Boeing", 20));
+            Console.WriteLine("Пунк назначения: Номер полёта: Название самолёта: Время полёта (час)");
+            foreach (string i in SpecCondition)
+                Console.WriteLine(i);
+            Console.WriteLine();
+
+            Console.WriteLine("Самый долгий полет");
+            IEnumerable<Airline> Long_flight = from z in Plane
+                                               where z.time > 10     
+            select z;
+            Console.WriteLine("Пунк назначения: Номер полёта: Название самолёта: Время полёта (час)");
+            foreach (Airline i in Long_flight)
+                Console.WriteLine(i);
+            Console.WriteLine();
+            Console.WriteLine("Расположение по алфавиту");
+            IEnumerable<Airline> Aphab = Plane
+                                         .OrderBy(i => i.destination);
+            Console.WriteLine("Пунк назначения: Номер полёта: Название самолёта: Время полёта (час)");
+            foreach (Airline i in Aphab)
+                Console.WriteLine(i);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Свой запрос:");
+            string[] str__1 = { "one", "two", "four" };
+            string[] str__2 = { "five", "six", "two" };
+            foreach (string x in str__1)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine();
+            foreach (string x in str__2)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Join");
+            string[] str_1 = new string[3] { "123", "456", "121" };
+            int[] num = new int[3] { 1, 2, 3 };
+
+            var SomeType = str
+                .Join(
+                num,
+                w => w.Length,
+                s => s,
+                (w, s) => new
+                {
+                    name = w,
+                    month = s
+                }
+                );
+            foreach(var x in SomeType)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}

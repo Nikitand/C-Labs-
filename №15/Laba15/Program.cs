db03dfddff6a6320910e4584f43c16ba129a0fa3
Nikitand <nikitos_by_brest@mail.ru>
2019-12-23 22:58:45 +0300

    finish

--- №15/Laba15/Program.cs
+++ №15/Laba15/Program.cs
@@ -15,20 +15,26 @@ namespace Laba15
         static void Main(string[] args)
         {
             
-            Procecs();
-           Domain();
+                Procecs();
+                Domain();
+            
+            int num = 0;
+            // устанавливаем метод обратного вызова
+            TimerCallback tm = new TimerCallback(Privet);
+            // создаем таймер
+            Timer timer = new Timer(tm, num, 0, 3000);
+
 
             Thread first_thread = new Thread(new ThreadStart(Task));
             first_thread.Name = "number_1";
             first_thread.Priority = ThreadPriority.Highest;
             first_thread.Start();
 
-              first_thread.Suspend();
-
+            first_thread.Suspend();
             var currt = Thread.CurrentThread;
             Console.WriteLine("info_about_thread");
-            Console.WriteLine("-" + currt.Name+ "--");
-            Console.WriteLine("Priority "+currt.Priority);
+            Console.WriteLine("-" + currt.Name + "--");
+            Console.WriteLine("Priority " + currt.Priority);
             Console.WriteLine(" Sostoinie " + currt.ThreadState);
             Console.WriteLine("Id " + currt.ManagedThreadId);
             if (currt.IsAlive)
@@ -39,19 +45,30 @@ namespace Laba15
             {
                 Console.WriteLine("not Background");
             }
-           first_thread.Resume();
-            
-            Console.WriteLine(  );
-            Thread second_thread = new Thread(new ThreadStart(Task_2));
-            Thread third_thread = new Thread(new ThreadStart(Task_3));
+            first_thread.Resume();
+
+            Console.WriteLine();
+            Thread.Sleep(5000); 
+
+            Console.WriteLine("Even_Odd");
+            StartInfo t1 = new StartInfo(50, 50, true, true);
+            StartInfo t2 = new StartInfo(50, 100, false, true);
+         
+            Thread second_thread = new Thread(new ParameterizedThreadStart(StaticClassMutex.WriteNumbers));
+            Thread third_thread = new Thread(new ParameterizedThreadStart(StaticClassMutex.WriteNumbers));
+            third_thread.Start(t2);
+            second_thread.Start(t1);
             second_thread.Name = "number_2";
             second_thread.Priority = ThreadPriority.AboveNormal;
             third_thread.Name = "number_3";
             third_thread.Priority = ThreadPriority.Normal;
-            Console.WriteLine("Even_Odd");
-            second_thread.Start();
-            third_thread.Start();
-            
+            Console.WriteLine(); ;
+            Thread.Sleep(10000);
+            third_thread = new Thread(new ParameterizedThreadStart(StaticClassBarrier.WriteNumbers));
+            second_thread = new Thread(new ParameterizedThreadStart(StaticClassBarrier.WriteNumbers));
+            third_thread.Start(t2);
+            second_thread.Start(t1);
+            Thread.Sleep(10000);
         }
 
         static void Procecs()
@@ -82,7 +99,7 @@ namespace Laba15
             Console.WriteLine("create domain");
             AppDomain my_domain = AppDomain.CreateDomain("My_Domain");
 
-           my_domain.Load(new AssemblyName("System.Data"));
+            //   my_domain.Load(new AssemblyName("System.Data"));
             Assembly[] assemblies_2 = my_domain.GetAssemblies();
             foreach (var x in assemblies_1)
             {
@@ -94,61 +111,66 @@ namespace Laba15
         }
         static void Task()
         {
-             
-                int n = 50;
-                int a = 0;
-                while (a != n)
-                {
-                    Thread.Sleep(100);
-                    a++;
-                    Console.Write(a + " ");
-                    using (StreamWriter fs = new StreamWriter("Count.txt", true, System.Text.Encoding.Default))
-                    {
-                        fs.Write(a + " ");
-                        fs.Close();
-                    }
 
-                }
-            
-        }
-            static void Task_2()
+            int n = 50;
+            int a = 0;
+            while (a != n)
             {
-
-            int count = 50;
-            int even = 0;
-           
-            while (even != count)
-            {
-                
-                Thread.Sleep(100);
-                Console.WriteLine(even);
-
-                using (StreamWriter fs = new StreamWriter("Even_Odd.txt", true, System.Text.Encoding.Default))
+                Thread.Sleep(50);
+                a++;
+                Console.Write(a + " ");
+                using (StreamWriter fs = new StreamWriter("Count.txt", true, System.Text.Encoding.Default))
                 {
-                    fs.Write(even);
+                    fs.Write(a + " ");
                     fs.Close();
                 }
-                even = even + 2;
+
             }
-            }
-        static void Task_3()
+
+        }
+        //static void Task_2()
+        //{
+
+        //    int count = 10;
+        //    int even = 0;
+
+        //    while (even != count)
+        //    {
+
+        //        Thread.Sleep(100);
+        //        Console.Write(even);
+
+        //        using (StreamWriter fs = new StreamWriter("Even_Odd.txt", true, System.Text.Encoding.Default))
+        //        {
+        //            fs.Write(even);
+        //            fs.Close();
+        //        }
+        //        even = even + 2;
+        //    }
+        //}
+        //static void Task_3()
+        //{
+        //    int count = 10;
+        //    int odd = 1;
+
+        //    while (odd != count)
+        //    {
+
+        //        Thread.Sleep(150);
+        //        Console.Write(odd);
+        //        using (StreamWriter fs = new StreamWriter("Even_Odd.txt", true, System.Text.Encoding.Default))
+        //        {
+        //            fs.Write(odd);
+        //            fs.Close();
+        //        }
+        //        odd = odd + 2;
+        //    }
+        //}
+
+
+        static void Privet(object num)
         {
-            int count = 51;
-            int odd = 1;
-           
-            while (odd != count)
-            {
-                
-                Thread.Sleep(150);
-                Console.WriteLine(odd);
-                using (StreamWriter fs = new StreamWriter("Even_Odd.txt", true, System.Text.Encoding.Default))
-                {
-                    fs.Write(odd);
-                    fs.Close();
-                }
-                odd = odd + 2;
-            }
+            Console.WriteLine("Повтор");
         }
-        
     }
 }
